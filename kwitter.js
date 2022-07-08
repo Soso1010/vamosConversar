441ddd51efdfc515d1c08fefa9f4096046237091
Soso1010 <99210584+Soso1010@users.noreply.github.com>
2022-07-08 18:18:12 -0300

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,4 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDSl5u7Y7jJ9BDwmfrumP6WbSdZWPqOPgo",
+  authDomain: "vamosconversar-da329.firebaseapp.com",
+  projectId: "vamosconversar-da329",
+  storageBucket: "vamosconversar-da329.appspot.com",
+  messagingSenderId: "660925630291",
+  appId: "1:660925630291:web:53e15ad5a1aa13b1e783a6"
+};
 
+// Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 function addUser() {
 
   userName = document.getElementById("userName").value;
