dcfb14a2e2a0a892d32b8d3943f47f732e07e652
Soso1010 <99210584+Soso1010@users.noreply.github.com>
2022-07-08 18:14:57 -0300

    Update kwitterRoom.js

--- kwitterRoom.js
+++ kwitterRoom.js
@@ -1,5 +1,16 @@
 
 //ADICIONE SEUS LINKS FIREBASE
+const firebaseConfig = {
+  apiKey: "AIzaSyDSl5u7Y7jJ9BDwmfrumP6WbSdZWPqOPgo",
+  authDomain: "vamosconversar-da329.firebaseapp.com",
+  projectId: "vamosconversar-da329",
+  storageBucket: "vamosconversar-da329.appspot.com",
+  messagingSenderId: "660925630291",
+  appId: "1:660925630291:web:53e15ad5a1aa13b1e783a6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
   userName = localStorage.getItem("userName");
 
